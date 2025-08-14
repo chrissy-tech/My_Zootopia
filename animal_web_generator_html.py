f3f54eebe2ecdc97fba03bc03c551a4e203c3b06
Christin <c.albrecht289@gmail.com>
2025-08-15 00:10:23 +0200

    Final Version

--- animal_web_generator_html.py
+++ animal_web_generator_html.py
@@ -5,8 +5,9 @@ with open("animals_template.html", "r", encoding="utf-8") as file:
     template = file.read()
 
 # Load the animal Data
-with open("animals_data.json", "r") as file:
+with open("animals_data.json", "r", encoding="utf-8") as file:
     animals_data = json.load(file)
+
 
 def serialize_animals(animals_data):
     output = "" # define an empty string
@@ -17,14 +18,27 @@ def serialize_animals(animals_data):
         type_animal = characteristics_animal.get("type")
         location_animal = animal.get("locations", [])
         first_location = location_animal[0] if location_animal else ""
+        weight_animal = characteristics_animal.get("weight",)
+        length_animal = characteristics_animal.get("length",)
+
         output += '<li class="cards__item">\n'
         output += f'    <div class="card__title">{name_animal}</div>\n'
-        output += '    <p class="card__text">'
-        output += f'<strong>Diet:</strong> {diet_animal}<br/>\n'
-        output += f'<strong>Location:</strong> {first_location}<br/>\n'
-        if type_animal:  # nur ausgeben, wenn vorhanden
-            output += f'<strong>Type:</strong> {type_animal}<br/>\n'
-        output += '    </p></li>\n'
+        output += '         <div class="card__text">\n'
+        output += '             <ul class="card__list">\n'
+        output += f'    <li class="card__list-item"><strong>Diet:</strong> {diet_animal}</li>\n'
+        output += f'    <li class="card__list-item"><strong>Location:</strong> {first_location}</li>\n'
+
+
+        """ output only when existing in json file """
+        if type_animal:
+            output += f'    <li class="card__list-item"><strong>Type:</strong> {type_animal}</li>\n'
+        if weight_animal:
+            output += f'    <li class="card__list-item"><strong>Weight:</strong> {weight_animal}</li>\n'
+        if length_animal:
+            output += f'    <li class="card__list-item"><strong>Length:</strong> {length_animal}</li>\n'
+        output += '             </ul>\n'
+        output += '         </div>\n'
+        output += '     </li>\n'
     return output
 
 
